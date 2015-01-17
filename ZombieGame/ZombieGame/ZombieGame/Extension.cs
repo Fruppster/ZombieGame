a60aa4f78cc9e39b0cd14895ef24f3388d29d630
Bino87 <kallenved@gmail.com>
2015-01-17 18:35:32 +0100

    Extension class

--- ZombieGame/ZombieGame/ZombieGame/Extension.cs
+++ ZombieGame/ZombieGame/ZombieGame/Extension.cs
@@ -1,11 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+
 
 namespace ZombieGame
 {
 	class Extension
 	{
+		static readonly Random Rand = new Random();
+
+		public static int Random(int _min, int _max)
+		{
+			return Rand.Next(_min, _max + 1);
+		}
+
+		public static int Random(int _max)
+		{
+			return Rand.Next(_max + 1);
+		}
 	}
 }
